fe2db5e0d25d87af6214ab1b1d3c6db396e245a2
のってぃ <74302768+notthei@users.noreply.github.com>
2024-09-05 11:50:56 +0900

    Add files via upload

---
+++ AutNuitka.py
@@ -0,0 +1,18 @@
+import os
+print('exe化するpyファイルのフルパスを入力')
+pyPath = input('>>')
+print('追加のオプションを選択')
+print('[1] 黒い画面を出さない')
+print('[2] Python環境がないところでも動くようにする')
+print('[3] ひとつのexeファイルにまとめる')
+print('[4] Tkinterをつかうおまじない')
+print('[5] *.icoファイルをアイコンとして使用')
+print('[6] なし')
+stType = int(input('>>'))
+stOption = ['--windows-disable-console',
+            '--standalone',
+            '--onefile',
+            '--enable-plugin=tk-inter',
+            '--windows-icon-from-ico="*.ico"',
+            '    ']
+os.system('nuitka ' + pyPath + ' '+stOption[stType-1])
